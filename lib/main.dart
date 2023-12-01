58e5d0e048f1ff2a02f6a0db1350f29c40a7006e
ElHbrouk <omar.elhbrouk2@gmail.com>
2023-12-01 15:33:24 +0200

    provide addNotesCubit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/cubits/add_notes_cubit/add_notes_cubit.dart';
 import 'package:notes_app/cubits/change_theme/change_theme_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/theme/app_theme.dart';
@@ -10,7 +11,7 @@ import 'package:notes_app/views/home_view.dart';
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
-  Hive.registerAdapter(NoteModelAdapter())
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const MyApp());
 }
 
@@ -21,8 +22,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => ChangeThemeCubit(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => ChangeThemeCubit(),
+        ),
+        BlocProvider(
+          create: (context) => AddNotesCubit(),
+        ),
+      ],
       child: BlocBuilder<ChangeThemeCubit, Brightness>(
         builder: (context, state) {
           return MaterialApp(
